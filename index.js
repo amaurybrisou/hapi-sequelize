d3817fbf678f4abcd7f227cf9c5fcc471b2c8a8e
amaurybrisou <amaury.brisou@gmail.com>
2014-07-24 16:15:02 +0200

    new tests

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 exports.register = function (plugin, options, next) {
 
   var Sequelize = require('sequelize'),
-      sequelize = new Sequelize(
-        options.dialect + ':' + options.storage, options)
-  
+    sequelize = new Sequelize(
+      options.dialect + ':' + options.storage, options)
+
   sequelize
     .authenticate()
-    .complete(function(err) {
+    .complete(function (err) {
       if (!!err) {
         plugin.log(['hapi-sequelize', 'error'], 'db.rdbms.squelize.authenticate')
         next(err)
       } else {
 
-        plugin.log(['hapi-sequelize', 'info'],'Sequelize connection has been established successfully.')
+        plugin.log(['hapi-sequelize', 'info'], 'Sequelize connection has been established successfully.')
 
       }
     })
 
-  plugin.servers.forEach(function(server){
-    plugin.expose('Sequelize', Sequelize)  
+  plugin.servers.forEach(function (server) {
+    plugin.expose('Sequelize', Sequelize)
     plugin.expose('sequelize', sequelize)
   })
-  
+
   return next()
 }
 
